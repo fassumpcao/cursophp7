418b56916dc1b10e1ea00ef0d16355a1aa6f8e72
fassumpcao <felipe.p.assumpcao@gmail.com>
2019-05-31 19:53:26 +0000

    Download de arquivos.

---
+++ exemplo_73_01.php
@@ -0,0 +1,18 @@
+<?php
+
+$link = "https://conteudo.imguol.com.br/c/home/layout/camaleao/web/logo.png";
+
+$content = file_get_contents($link);
+
+$parse = parse_url($link);
+
+$basename = basename($parse["path"]);
+
+$file = fopen($basename, "w+");
+
+fwrite($file, $content);
+fclose($file);
+
+ ?>
+
+ <img src="<?=$basename?>">
